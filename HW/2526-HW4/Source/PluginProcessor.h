7430d69765ad103a16e48a69dcf01aeeef26bf93
adityarpawar1902 <apawar43@gatech.edu>
2026-05-02 20:54:16 -0400

    Update PluginProcessor.h

--- HW/2526-HW4/Source/PluginProcessor.h
+++ HW/2526-HW4/Source/PluginProcessor.h
@@ -53,6 +53,8 @@ public:
     //==============================================================================
     void getStateInformation (juce::MemoryBlock& destData) override;
     void setStateInformation (const void* data, int sizeInBytes) override;
+    
+    juce::AudioProcessorValueTreeState apvts;
 
 
 private:
@@ -60,6 +62,11 @@ private:
     JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR (_2526HW4AudioProcessor)
 
     Delay delay;
+    
+    double samplingRate;
+    int bufferSize;
+    
     const int maxDelaySec = 5;
     
+    juce::AudioProcessorValueTreeState::ParameterLayout createParams();
 };
